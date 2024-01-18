5745a8e86875dac34c1b2e114a1ac8bb9347fdd4
binary-river <66468384+binary-river@users.noreply.github.com>
2024-01-18 17:39:25 +0900

    test code update

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 import java.util.Iterator;
@@ -10,21 +11,59 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class SbbApplicationTests {
 
 	@Autowired
 	private QuestionRepository questionRepository;
+	
+	@Autowired
+	private AnswerRepository answerRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Transactional
 	@Test
 	void testJpa() {
 		
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
 		
+		List<Answer> answerList = oq.get().getAnswerList();
+		for(Answer a:answerList) {
+			System.out.println( a.getId()+ ", "+ a.getContent());
+		}
+		
+		/*
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		
+		Answer a = new Answer();
+		a.setContent("sbb is just a small project");
+		a.setCreateDate(LocalDateTime.now());
+		a.setQuestion(oq.get());
+		this.answerRepository.save(a);
+		*/
+		
+		/*
+		assertEquals(3, this.questionRepository.count());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		this.questionRepository.delete(oq.get());
+		assertEquals(2, this.questionRepository.count());
+		*/
+		
+		/*
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("modified, what was sbb?" );
+		this.questionRepository.save(q);
+		*/
 		
 		/*
 		List<Question> r = this.questionRepository.findBySubjectLike("%sbb%");
